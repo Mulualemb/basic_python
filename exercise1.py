83522df1bfe25acd9216d66844500b8f3926f2e5
Mulualemb <elibogala@gmail.com>
2022-03-09 15:03:37 +0200

    first commit

---
+++ exercise1.py
@@ -0,0 +1,17 @@
+
+#q1
+
+# n = 20
+# while n > 0:
+#     if n % 2 != 0:
+#         print(n)
+#     n = n - 1
+
+
+
+# #q2
+#
+# for i in range(20+1):
+#     if i % 2 != 0:
+#         print(i)
+#     i = i - 1
