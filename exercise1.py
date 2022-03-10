3db324ffc2dbf3d9cb853b2cbaae7b4204c26e69
Mulualemb <elibogala@gmail.com>
2022-03-10 09:42:46 +0200

    5th change

--- exercise1.py
+++ exercise1.py
@@ -1,7 +1,7 @@
 
 #q1
 
-# n = 20
+# n = int(input("type a number:"))
 # while n > 0:
 #     if n % 2 != 0:
 #         print(n)
@@ -9,16 +9,72 @@
 
 
 
-# #q2
-#
-# for i in range(20+1):
-#     if i % 2 != 0:
-#         print(i)
-#     i = i - 1
+
+
+#q2
+# x=300
+# gues
+
+
 
 #q3
-# n = int(input("select a number:"))
-# while n > 0:
-#     if n % 2 != 0:
-#         print(n)
-#     n = n - 1
+# num1=int(input("please enter a number:"))
+# num2=int(input("please enter a number:"))
+# num3=int(input("please enter a number:"))
+# num4=int(input("please enter a number:"))
+# num5=int(input("please enter a number:"))
+# print((num1+num2+num3+num4+num5)/5)
+
+
+
+
+#q4
+# num = int(input("please insert a number:"))
+# num = num**2
+# print(num)
+
+
+
+#q5
+# age = int(input("please enter your age:"))
+# age = age + 20
+# print("age in 20 years", age)
+
+
+
+#q6
+# amount = int(input("please enter the amount:"))
+# amount_tax = amount/100*17
+# print("price with tax" ,amount+amount_tax)
+# print("price without tax " ,amount)
+
+
+
+#q7
+# num1 = int(input("insert a number:"))
+# num2 = int(input("insert a number:"))
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+
+
+
+
+
+
+#q8
+# num1 = int(input("please enter a number,"))
+# num2 = int(input("please enter a number,"))
+# num3 = int(input("please enter a number,"))
+# num4 = int(input("please enter a number,"))
+# print((num1+num2+num3+num4)/4)
+
+
+
+#q9
+# nails = int(input("please insert number of nails:"))
+# print("number of boxes is",nails//40)
+
+
+
